d8cbd5cc4cedbb1f4f4d13d9f3c05bf054d6ac72
wenner <nelsondiasdemedeiros@gmail.com>
2020-07-30 12:28:09 -0300

    feat(PaymentMethod): create PaymentMethod

--- payment_gateway/models.py
+++ payment_gateway/models.py
@@ -8,4 +8,19 @@ class PaymentMethodEnum(Enum):
 
 payment_method_choices = [(pm.value, pm.name) for pm in PaymentMethodEnum]
 
+class PaymentMethod(AutoCreateUpdatedMixin):
+    PAYMENT_METHOD_CHOICES = (
+        ("credit_card", "credit_card"),
+        ("bank_slip", "billet")
+    )
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, choices=PAYMENT_METHOD_CHOICES, verbose_name='name')
+    allow_installments = models.BooleanField(default=True, verbose_name='with installments')
+
+    class Meta:
+        verbose_name = 'payment method'
+
+    def __str__(self):
+        return self.get_name_display()
+    
