0a6278d8c15486f1bbd375b2d3818a189a5c1ff1
wenner <nelsondiasdemedeiros@gmail.com>
2020-07-30 18:13:33 -0300

    feat(payment_gateway/serializer): created PaymentMethodSerializer

--- payment_gateway/serializers.py
+++ payment_gateway/serializers.py
@@ -2,7 +2,11 @@ from payment_gateway.models import PagarmeGateway, PaymentMethod
 from rest_polymorphic.serializers import PolymorphicSerializer
 from rest_framework import serializers
 
-
+class PaymentMethodSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = PaymentMethod
+        fields = '__all__' 
 
 class PagarmeGatewaySerializer(serializers.ModelSerializer):
 
