8111f46d5231ce7bce1c9dd113accb4ad325cee8
wenner <nelsondiasdemedeiros@gmail.com>
2020-07-30 12:20:50 -0300

    feat(PaymentMethodEnum): create PaymentMethod

--- payment_gateway/models.py
+++ payment_gateway/models.py
@@ -1,3 +1,11 @@
+from common.models import AutoCreateUpdatedMixin
 from django.db import models
+from enum import Enum
 
-# Create your models here.
+class PaymentMethodEnum(Enum):
+    CREDIT_CARD = 'credit_card'
+    BANK_SLIP = 'bank_slip'
+
+payment_method_choices = [(pm.value, pm.name) for pm in PaymentMethodEnum]
+
+
