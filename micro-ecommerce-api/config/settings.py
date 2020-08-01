7bec7ba0dc559d0400dc630809a4d9dfae393d31
wenner <nelsondiasdemedeiros@gmail.com>
2020-08-01 17:39:20 -0300

    chore(settings): add environment broker

--- micro-ecommerce-api/config/settings.py
+++ micro-ecommerce-api/config/settings.py
@@ -15,15 +15,21 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+env_path = os.path.join(BASE_DIR, '.env')
+
+if os.path.exists(env_path):
+    environ.Env.read_env(env_path)
+else:
+    print("Warning: Dotenv does not exist")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!71mw7$cvbm$_9i$xo09w!1ul*_dd+_-nqwotrqwtqynjk1zxu'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -86,12 +92,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': env.db()
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -158,6 +160,8 @@ MEDIA_URL =  '/media/'
 MEDIA_BASE_PATH = 'uploads'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+CELERY_BROKER_URL = env('RABBITMQ_URI')
 
 '''
 Config JWT
