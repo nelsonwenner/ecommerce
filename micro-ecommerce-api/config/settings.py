b6e1661bcb96fabb272d0b5fb9db1ab1c87d205c
wenner <nelsondiasdemedeiros@gmail.com>
2020-08-01 17:50:26 -0300

    chore(import environ): add import environ

--- micro-ecommerce-api/config/settings.py
+++ micro-ecommerce-api/config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+import environ
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
