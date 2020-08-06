6336e6a7aec6074d42dd7e945c60c3e0453eb82a
wenner <nelsondiasdemedeiros@gmail.com>
2020-08-06 19:51:42 -0300

    feat(Cart): add sum product selected and quantity

--- frontend/src/components/theme/Header/Drop/Cart/index.jsx
+++ frontend/src/components/theme/Header/Drop/Cart/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './styles.css';
 
+import { useCart } from '../../../../../providers/CartProvider';
+
 const Cart = () => {
+  const [cart] = useCart();
+  
   return (
     <div className="cart">
       <span className="icon-cart"></span>
-      <span className="count-cart">0</span>
+      <span className="count-cart">{ cart.length }</span>
       <span className="name">Cart</span>
-      <td className="total">$ 0,00</td>
+      <td className="total">$ { cart.reduce((acc, current) => acc + (current.price * current.quantity), 0) } ,00</td>
     </div>
   )
 }
