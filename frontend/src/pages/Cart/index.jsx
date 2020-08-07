f18dcf7e8dd3844ee1760ceb3a7cb19ace19db89
wenner <nelsondiasdemedeiros@gmail.com>
2020-08-07 11:48:46 -0300

    feat(Cart): create Cart Component and added route /cart

---
+++ frontend/src/pages/Cart/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+import Layout from '../../components/common/Layout';
+
+const Cart = () => {
+  return (
+    <Layout>
+      
+    </Layout>
+  )
+}
+
+export default Cart;
--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 
+import Cart from '../pages/Cart';
 import Home from "../pages/Home";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Route component={ Home } path="/" exact />
+      <Route component={ Cart } path="/cart" exact />
     </BrowserRouter>
   );
 };
