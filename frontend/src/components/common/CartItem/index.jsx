1532d3eeff375398dbf6d6e0af0120a36e3d5016
wenner <nelsondiasdemedeiros@gmail.com>
2020-08-07 20:46:22 -0300

    feat(cart item): create cartitem from product

---
+++ frontend/src/components/common/CartItem/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './styles.css';
+
+import { useCart } from '../../../providers/CartProvider';
+
+const CartItem = () => {
+  const { cart } = useCart();
+
+  return (
+    <>
+      {cart.map((product, index) => (
+        <div className="cart-item" key={ index }>
+          <div className="cart-img">
+            <img src={ `${process.env.REACT_APP_ECOMMERCE_API_URL}${ product.image_url }` } alt="cart-item" />
+            <h4>{ product.title }</h4>
+          </div>
+          <span>$ { product.price }</span>
+        </div>
+      ))}
+    </>
+  )
+}
+
+export default CartItem;
--- frontend/src/pages/Cart/index.jsx
+++ frontend/src/pages/Cart/index.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import './styles.css';
 
+import CartItem from '../../components/common/CartItem';
 import Layout from '../../components/common/Layout';
 
 const Cart = () => {
   return (
     <Layout>
-      
+      <div className="cart-main">
+        <div className="container cart-main">
+          <h4 className="path-page">Home / Cart</h4>
+          <h4 className="cart-title">Cart</h4>
+          <div className="row">
+            <div className="column xlarge-8 large-12 medium-12 small-12" >
+              <div className="cart-table-header mt-30">
+                <div className="product">Product</div>
+                <div className="price">Price</div>
+              </div>
+              <CartItem />
+            </div>
+            <div className="column xlarge-4 large-12 medium-12 small-12">
+              <div className="cart-summary mt-30">
+                <h4 className="order-summary">Order Summary</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
