fb9ae2717358728f23ea2fabc6d8168f3b3e375f
wenner <nelsondiasdemedeiros@gmail.com>
2020-08-11 11:58:21 -0300

    feat(hooks): create usePersistedAuth hooks

---
+++ frontend/src/hooks/usePersistedAuth.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+const usePersistedAuth = (key) => {
+  const [auth, setAuth] = useState(() => {
+    const storageValue = localStorage.getItem(key);
+    
+    if (storageValue) {
+      return JSON.parse(storageValue);
+    } else {
+      return {};
+    } 
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(auth));
+  }, [key, auth]);
+
+  return [auth, setAuth];
+}
+
+export default usePersistedAuth;
---
+++ frontend/src/providers/AuthProvider.js
@@ -0,0 +1,1 @@
+import React, { createContext, useContext } from 'react';
