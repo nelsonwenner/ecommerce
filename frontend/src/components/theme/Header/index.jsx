421797bb36cea4e4dd454eb8914355162cd53eba
wenner <nelsondiasdemedeiros@gmail.com>
2020-08-20 15:12:29 -0300

    feat(Header): added fixed-top in Header Component

--- frontend/src/components/theme/Header/index.jsx
+++ frontend/src/components/theme/Header/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './styles.css';
 
 import Navbar from './Navbar';
 import Drop from './Drop';
 
 const Header = () => {
   return (
-    <div className="header">
+    <div className="fixed-top">
       <Navbar />
       <Drop />
     </div>
