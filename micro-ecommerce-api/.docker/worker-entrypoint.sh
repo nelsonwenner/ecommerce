cd37b457869c129ac05d29a5f7564d47585cffd6
wenner <nelsondiasdemedeiros@gmail.com>
2020-12-28 11:53:12 -0300

    refactor: wait database in worker

--- micro-ecommerce-api/.docker/worker-entrypoint.sh
+++ micro-ecommerce-api/.docker/worker-entrypoint.sh
@@ -4,6 +4,6 @@ cd /home/django/app
 
 pip install -r requirements.txt
 
-dockerize -wait tcp://app:8000 -timeout 2700s -wait-retry-interval 10s
+dockerize -wait tcp://database:5432 -wait tcp://app:8000 -timeout 2700s -wait-retry-interval 10s
 
 celery worker -B -l info -A config.celery -s /tmp/celerybeat-schedule
