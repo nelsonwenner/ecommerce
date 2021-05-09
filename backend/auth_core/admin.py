48e895739b82b7094694a92a491ebb6f8c9227e0
wenner <nelsondiasdemedeiros@gmail.com>
2021-05-09 00:56:05 -0300

    refactor: add fields

--- backend/auth_core/admin.py
+++ backend/auth_core/admin.py
@@ -1,9 +1,27 @@
-from django.contrib.auth.models import Group as DjangoGroup
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from my_admin.admin import admin_site
 from auth_core.models import User
 
 class UserAdmin(DjangoUserAdmin):
-    pass
+    fieldsets = (
+        (None, {'fields': ('username', 'password','email')}),
+        (('Personal info'), {'fields': ('first_name', 'last_name',)}),
+        (('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+        'groups', 'user_permissions')}),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2')}
+        ),
+    )
+
+    form = UserChangeForm
+    add_form = UserCreationForm
+    search_fields = ('email',)
+    filter_horizontal = ('groups', 'user_permissions',)
 
 admin_site.register(User, UserAdmin)
--- backend/auth_core/apps.py
+++ backend/auth_core/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AuthCoreConfig(AppConfig):
     name = 'auth_core'
-    verbose_name = 'Authentication and Authorization'
+    verbose_name = 'User Authentication and authorization'
