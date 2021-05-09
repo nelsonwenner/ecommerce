0d90f2f91c1b6642efb64069392ba7509d7e87c3
wenner <nelsondiasdemedeiros@gmail.com>
2021-05-09 00:54:15 -0300

    refactor: remove UserClient

--- backend/auth_core/models.py
+++ backend/auth_core/models.py
@@ -1,23 +1,21 @@
 from django.contrib.auth.models import AbstractUser
-from auth_core.managers import UserClientManager
+from auth_core.managers import UserManager
+from django.utils import timezone
 from django.db import models
 import uuid
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     username = models.CharField(max_length=30)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, error_messages={
+        'unique': ("A user with that email already exists."),
+    })
 
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
 
     class Meta:
         verbose_name = 'user auth'
-
-class UserClient(User):
-    objects = UserClientManager()
-
-    class Meta:
-        proxy = True
-        verbose_name = 'client'
-
