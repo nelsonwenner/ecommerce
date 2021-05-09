d377ab45ac47f820db4d5200a896fc7ac5433a20
wenner <nelsondiasdemedeiros@gmail.com>
2021-05-09 00:53:39 -0300

    refactor: add UserManager

--- backend/auth_core/managers.py
+++ backend/auth_core/managers.py
@@ -1,7 +1,31 @@
 from django.contrib.auth.models import UserManager
 
-class UserClientManager(UserManager):
-    
-    def create_client(self, username, email=None, password=None, **extra_fields):
-        user = super().create_user(username, email, password, **extra_fields)
+class UserManager(UserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
         return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+
+        return self._create_user(email, password, **extra_fields)
