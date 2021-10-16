2dcc61aa09c6831d8feabc4898de1c8649343d8e
wenner <nelsondiasdemedeiros@gmail.com>
2021-10-15 23:11:51 -0300

    chore: remove frontend

--- frontend/src/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './styles.css';
-import Routes from './routes/routes';
-
-const App = () => <Routes />
-
-export default App;
--- frontend/src/components/common/Layout/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components'
-
-export const Wrapper = styled.div`
-  width: 100%;
-  margin-bottom: 1rem;
-`
